e852e156c5bc20df7a8c2ba5f43daa60cdcd1c13
Stephen Gordon <sgordon@redhat.com>
2018-10-03 16:44:21 -0400

    Set correct local storage permissions for CDI
    When using CDI with local storage QEMU needs to be able to access the
    files backing the PVCs on the host.
    
    See also: https://github.com/kubevirt/containerized-data-importer/issues/452

--- workarounds.sh
+++ workarounds.sh
@@ -10,3 +10,6 @@ echo /var/lib/kubelet/device-plugins
 ls /var/lib/kubelet/device-plugins
 echo $KUBELET_ROOTFS/var/lib/kubelet/device-plugins
 sudo ls $KUBELET_ROOTFS/var/lib/kubelet/device-plugins
+
+# Workaround for https://github.com/kubevirt/containerized-data-importer/issues/452
+sudo setfacl -m user:107:rwx /var/lib/minishift/base/openshift.local.pv/pv*
