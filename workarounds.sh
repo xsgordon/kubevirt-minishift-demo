ae929cbc7d63ad3b19a525ed011f000a086f31d2
Stephen Gordon <sgordon@redhat.com>
2018-10-12 16:02:25 -0400

    Drop workaround for missing /dev/tun and /dev/kvm
    OKD 3.11 includes the fix for https://github.com/openshift/origin/pull/20351 so
    the relevant workaround is no longer required (and in fact appears to actually
    break the fix).

--- workarounds.sh
+++ workarounds.sh
@@ -1,15 +1,2 @@
-# Workaround for https://github.com/openshift/origin/pull/20351
-sed "/kubeletArguments/ a\  feature-gates:" \
-    /var/lib/minishift/openshift.local.config/node-localhost/node-config.yaml
-sed "/feature-gates/ a\  - DevicePlugins: true" \
-    /var/lib/minishift/openshift.local.config/node-localhost/node-config.yaml
-KUBELET_ROOTFS=`echo $(docker inspect $(docker ps | grep kubelet | cut -d" " -f1) | grep MergedDir | cut -d":" -f2 | grep -E -o "/[a-zA-Z0-9\/]*")`
-sudo mkdir -p /var/lib/kubelet/device-plugins $KUBELET_ROOTFS/var/lib/kubelet/device-plugins
-sudo mount -o bind $KUBELET_ROOTFS/var/lib/kubelet/device-plugins /var/lib/kubelet/device-plugins
-echo /var/lib/kubelet/device-plugins
-ls /var/lib/kubelet/device-plugins
-echo $KUBELET_ROOTFS/var/lib/kubelet/device-plugins
-sudo ls $KUBELET_ROOTFS/var/lib/kubelet/device-plugins
-
 # Workaround for https://github.com/kubevirt/containerized-data-importer/issues/452
 sudo setfacl -m user:107:rwx /var/lib/minishift/base/openshift.local.pv/pv*
